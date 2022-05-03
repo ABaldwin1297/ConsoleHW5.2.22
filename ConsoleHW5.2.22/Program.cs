60073ff47ad33fecd02fd908595e51026d496d42
Andrew.Baldwin <Andrew.Baldwin@IVP-LDDQ12MG3>
2022-05-03 09:41:14 -0400

    Add project files.

---
+++ ConsoleHW5.2.22/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int count1 = 10;
+int count2 = 4;
+
+Console.WriteLine($"Count 1 = 10");
+
+Console.WriteLine($"Count 2 = 4"); 
+
+int count3 = (count1 + count2);
+Console.WriteLine($"Count 1 + Count 2 = {count3}");
+
+int count4 = (count1 - count2);
+Console.WriteLine($"Count 1 - Count 2 = {count4}");
+
+int count5 = (count1 / count2);
+Console.WriteLine($"Count 1 / Count 2 = {count5}");
+
+int count6 = (count1 * count2);
+Console.WriteLine($"Count 1 * Count 2 = {count6}");
+
+Console.WriteLine($"Sum of all math problems = {count3 + count4 + count5 + count6}");
